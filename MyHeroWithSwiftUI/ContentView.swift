bf03b0bf8043834a35293b77a1d8a3883bed71ac
gusadi <gusadi872@gmail.com>
2020-07-02 22:17:59 +0800

    Initial Commit

---
+++ MyHeroWithSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyHeroWithSwiftUI
+//
+//  Created by Agus Adi Pranata on 02/07/20.
+//  Copyright © 2020 Dicoding Indonesia. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
