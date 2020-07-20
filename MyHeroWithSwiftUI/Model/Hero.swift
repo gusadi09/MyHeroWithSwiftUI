5f35c8d8e0d360a308b30e70bc81614453a8577b
gusadi <gusadi872@gmail.com>
2020-07-21 00:46:05 +0800

    .

---
+++ MyHeroWithSwiftUI/Model/Hero.swift
@@ -0,0 +1,16 @@
+//
+//  Hero.swift
+//  MyHeroWithSwiftUI
+//
+//  Created by Agus Adi Pranata on 02/07/20.
+//  Copyright © 2020 Dicoding Indonesia. All rights reserved.
+//
+
+import UIKit
+
+struct Hero: Identifiable {
+    let id: Int
+    let photo: String
+    let name: String
+    let description: String
+}
